dc20aed91260ac43a0a6da1a3e51c506c4d125f0
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-19 15:17:14 +0600

    make server doen

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+const port = process.env.PORT|| 5000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.listen(port,()=>{
+    console.log('listening to port',port)
+  })
+
