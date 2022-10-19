c136827e26ae9369a5ee3dbcc7fc6aadec10ce82
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-19 15:27:04 +0600

    done setup server add push data

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const cors = require('cors')
 app.use(cors())
 const port = process.env.PORT|| 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+// get all data
+const categories = require('./data/categories.json')
+const news = require('./data/news.json')
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+  })
+
+app.get('/news', (req, res) => {
+    res.send(news)
   })
 
   app.listen(port,()=>{
